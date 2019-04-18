8d60a46cf93c06e473fba39ed97b8d04866d305c
Adrian Rodriguez <aropie@hotmail.com>
2019-04-17 22:39:12 -0500

    Fix: maze drawing now works correctly

--- Mazes.hs
+++ Mazes.hs
@@ -27,7 +27,7 @@ neighbors g (x,y) = filter p $ foldr (\(a,b) acc -> (a+x,b+y):acc) [] [(0,1),(0,
 
 drawCell :: Grid -> Coord -> String
 drawCell g (x,y) = (if down `S.member` connected then "  " else "__") ++
-                   (if right `S.member` connected then "" else "|")
+                   (if right `S.member` connected then "_" else "|")
   where
     connected = g M.! (x,y)
     down = (x,y-1)
@@ -39,9 +39,8 @@ drawRow g r = foldr (++) "" [drawCell g (x,r) | x <- [0..max_x]]
     max_x = fst $ getBounds g
 
 drawMaze :: Grid -> String
-drawMaze g = foldl (++) top_wall ["|" ++ (drawRow g r) ++ "\n" | r <- [0..max_y]]
+drawMaze g = foldl (++) top_wall ["|" ++ (drawRow g r) ++ "\n" | r <- [max_y,max_y-1..0]]
   where
     max_y = snd $ getBounds g
     max_x = (fst $ getBounds g) + 1
-    -- top_wall = " " ++ replicate (max_x * 3) '_' ++ "\n"
     top_wall = " " ++ (foldr (++) "" $ replicate max_x "__ ") ++ "\n"
